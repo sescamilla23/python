9a6077fa63863e450c9fb2611e9ca04cf2a3672e
sescamilla23 <141870509+sescamilla23@users.noreply.github.com>
2023-08-11 15:50:30 -0500

    test_account v2

--- test_account.py
+++ test_account.py
@@ -3,6 +3,7 @@ import unittest
 
 
 class TestAccount(unittest.TestCase):
+
     def test_init(self):
         account1 = Account("John")
         self.assertEqual(account1.get_name(), "John")
@@ -10,11 +11,16 @@ class TestAccount(unittest.TestCase):
 
     def test_deposit(self):
         account1 = Account("John")
+        assert account1.deposit(-1) is False
+        assert account1.deposit(0) is False
         account1.deposit(50)
         self.assertEqual(account1.get_balance(), 50)
 
     def test_withdraw(self):
         account1 = Account("John")
+        assert account1.withdraw(-1) is False
+        assert account1.withdraw(0) is False
+        assert account1.withdraw(200) is False
         account1.deposit(200)
         account1.withdraw(100)
         self.assertEqual(account1.get_balance(), 100)
